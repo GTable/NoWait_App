0ba7db800f92d637ed9f2c3935711b7ab55ff1a9
oriNuguri25 <byeongjun25@naver.com>
2025-12-08 16:44:37 +0900

    feat: 로그인 UI 완성, 공통 버튼 컴포넌트, 공통 캐러셀 슬라이드 컴포넌트

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -1,12 +1,34 @@
+import { ScreenLayout } from "@/app/layout/ScreenLayout";
+import { OnboardSlide } from "@/features/login/components/OnboardSlide";
+import { Button } from "@/shared/ui/Button";
+import styled from "@emotion/native";
 import React from "react";
-import { Text, View } from "react-native";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <ScreenLayout>
+      <E.Container>
+        <OnboardSlide />
+      </E.Container>
+      <E.ButtonContainer>
+        <Button variant="kakao" />
+      </E.ButtonContainer>
+    </ScreenLayout>
   );
 };
 
 export default LoginScreen;
+
+const E = {
+  Container: styled.View({
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }),
+
+  ButtonContainer: styled.View({
+    width: "100%",
+    paddingHorizontal: 25,
+    paddingVertical: 32,
+  }),
+};
