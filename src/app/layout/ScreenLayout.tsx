d4a433ecb08b79c0021534b47f94a817d8cd8feb
oriNuguri25 <byeongjun25@naver.com>
2025-12-08 18:22:45 +0900

    feat: expo prebuild 실행, emotion 코드 수정

--- src/app/layout/ScreenLayout.tsx
+++ src/app/layout/ScreenLayout.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ReactNode } from "react";
 import { colors } from "../styles/colors";
+import styled from "@emotion/native";
 
 interface ScreenLayoutProps {
   children: ReactNode;
@@ -8,9 +9,12 @@ interface ScreenLayoutProps {
 
 // 화면을 만들 때 SafeAreaView로 감싸주는 레이아웃 컴포넌트입니다.
 export const ScreenLayout = ({ children }: ScreenLayoutProps) => {
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.white[100] }}>
-      {children}
-    </SafeAreaView>
-  );
+  return <E.MainLayout>{children}</E.MainLayout>;
+};
+
+const E = {
+  MainLayout: styled.View({
+    flex: 1,
+    backgroundColor: colors.white[100],
+  }),
 };
