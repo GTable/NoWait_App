c8ab17bd57f31c8e4424d75eca94535fb81ba2dc
oriNuguri25 <byeongjun25@naver.com>
2025-12-16 15:11:27 +0900

    feat: 로그인 기능 구현 및 환경변수 읽을 수 있도록 추가

---
+++ src/shared/api/axios.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const SERVER_URI = process.env.SERVER_URI;
+
+export const api = axios.create({
+  baseURL: SERVER_URI?.replace(/\/$/, ""),
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터: 요청 전 헤더 추가 및 전처리
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터: 응답 데이터 추출 및 에러 메시지 처리
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const errorMessage = error.response?.data?.message || error.message;
+    return Promise.reject(new Error(errorMessage));
+  }
+);
